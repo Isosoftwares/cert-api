86ac1c2c805bf44c9800537d8e3459b622192f40
peter514 <peterkgachora1@gmail.com>
2025-04-29 15:51:11 +0300

    fix

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -3,11 +3,9 @@ const allowedOrigins = [
   "http://localhost:5174",
   "http://192.168.101.105:5173",
   "https://zyftpay.com",
-  "https://www.zyftpay.com",
-  "https://api.fafullz.com",
-  "https://admin.vshop01.com",
-  "http://192.168.101.105:5174",
   "http://localhost:3502",
+  "https://cert.isosoftwares.com",
+  "https://certadmin.isosoftwares.com",
 ];
 
 module.exports = allowedOrigins;
