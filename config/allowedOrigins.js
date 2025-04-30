a999fa7a5d889234d0fe776b201f5517195f5046
peter514 <peterkgachora1@gmail.com>
2025-04-30 17:51:38 +0300

    fix origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,10 +2,9 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://192.168.101.105:5173",
-  "https://zyftpay.com",
   "http://localhost:3502",
-  "https://cert.isosoftwares.com",
-  "https://certadmin.isosoftwares.com",
+  "https://crendential.net",
+  "https://admin.crendential.net",
 ];
 
 module.exports = allowedOrigins;
